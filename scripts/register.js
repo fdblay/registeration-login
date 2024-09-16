8f23fc24762414c571fef3b541f62cc1b24d6090
FrederickKumah <fdblay@gmail.com>
2024-09-16 11:42:15 +0000

    my first commit; this includes html and javascript files

---
+++ scripts/register.js
@@ -0,0 +1,11 @@
+// Select form by name
+const registerForm = document.getElementById('register-form');
+
+// Handle form submit event
+registerForm.onsubmit = function(){
+    // collect username
+    // collect email
+    // collect password
+    // save user information
+    // send confirmation email
+}
